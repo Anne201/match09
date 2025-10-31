f3e8258f1f4e62820b210017ae07759ab8d357eb
Anne201 <118590269+Anne201@users.noreply.github.com>
2025-10-31 14:38:57 +0800

    V2

--- match09/match_menu_set.js
+++ match09/match_menu_set.js
@@ -1,6 +1,7 @@
 menu_title='對對碰';
 menu_items = new Array(
-  '不蓋牌,match_set.js'
-, '記憶大考驗,match_set_memory.js'
-, '九九乘法,match_set_9x9.js'
+  '聲符對對碰,match_set_01.js'
+, '韻符對對碰,match_set_02.js'
+, 'ㄧ的結合韻對對碰,match_set_03.js'
+, 'ㄨ和ㄩ的結合韻對對碰,match_set_04.js'
 );
---
+++ match09/match_set_02.js
@@ -0,0 +1,117 @@
+//=======================================================
+// HTML5 對對碰 題庫設定檔
+//=======================================================
+
+//------------------------------
+//遊戲標題
+//------------------------------
+title = '聲符對對碰';
+
+//------------------------------
+//卡片上文字的字體名稱
+//------------------------------
+font = 'EngTRESS A';
+
+//------------------------------
+//是否顯示格子的編號
+//  true :顯示編號   false :無編號
+//------------------------------
+showNumber = false;
+
+//------------------------------
+//編號大小(相對於格子的比例)
+//------------------------------
+numberLabelFontSizeScale = 0.4;
+
+//------------------------------
+//答對時加多少分
+//------------------------------
+scoreAdd = 100;
+
+//------------------------------
+//答錯時扣多少分
+//------------------------------
+scoreMinus = 150;
+
+//------------------------------
+//是否覆蓋牌
+//  true :蓋牌   false :不蓋牌
+//------------------------------
+memoryMode = false;
+
+//------------------------------
+//等幾秒才蓋牌
+//------------------------------
+momorySeconds = 10;
+
+//------------------------------
+//牌被選取時的邊框顏色
+//------------------------------
+card_selected_border_color = '#ff0000';
+
+//------------------------------
+//文字的顏色
+//------------------------------
+card_text_color = '#ff6600';
+
+//------------------------------
+//蓋牌時背面的圖案或文字
+//------------------------------
+card_back = 'assets/smile.png';
+
+//------------------------------
+//牌未選取時的背景顏色(可以設多個,用逗號分隔)
+//------------------------------
+card_back_color = '#006666';
+
+//------------------------------
+//牌未選取時文字的顏色
+//------------------------------
+card_back_text_color = '#ffffff';
+
+//------------------------------
+//Google TTS 文字轉語音的設定
+//語言代碼:  'en-US' 'zh-TW'
+//------------------------------
+tts_language = 'en-US';
+
+//語音的速度 0 ~ 1 (可用小數)
+tts_speed = 0.75;  
+
+//---------------------------------------------------
+// [題庫設定]
+//---------------------------------------------------
+
+//------------------------------
+//題庫欄位分隔符號
+//------------------------------
+fields_seperator = '##';
+
+//------------------------------
+//一張卡片多個內容的分隔符號
+//------------------------------
+fields_seperator2 = '~~';
+
+//
+// [題目設定]
+//
+// 每一行即是一個問題(一對牌組)
+// 每一個問題中以題庫欄位分隔符號分為兩欄(兩張牌)
+// 牌卡目前支援的素材有：文字、圖片、聲音
+//
+// 一張卡片可以指定多個內容
+// 多內容用 fields_seperator2(~~) 分隔，會用亂數取用其中一個
+//
+questionLines = function(){/*--這一行請勿更改--
+https://anne201.github.io/match09/match09/samples012/ㄐ.png##https://anne201.github.io/match09/match09/samples012/ㄐ.mp3
+https://anne201.github.io/match09/match09/samples012/ㄑ.png##https://anne201.github.io/match09/match09/samples012/ㄑ.mp3
+https://anne201.github.io/match09/match09/samples012/ㄒ.png##https://anne201.github.io/match09/match09/samples012/ㄒ.mp3
+https://anne201.github.io/match09/match09/samples012/ㄓ.png##https://anne201.github.io/match09/match09/samples012/ㄓ.mp3
+https://anne201.github.io/match09/match09/samples012/ㄔ.png##https://anne201.github.io/match09/match09/samples012/ㄔ.mp3
+https://anne201.github.io/match09/match09/samples012/ㄕ.png##https://anne201.github.io/match09/match09/samples012/ㄕ.mp3
+https://anne201.github.io/match09/match09/samples012/ㄖ.png##https://anne201.github.io/match09/match09/samples012/ㄖ.mp3
+https://anne201.github.io/match09/match09/samples012/ㄗ.png##https://anne201.github.io/match09/match09/samples012/ㄗ.mp3
+https://anne201.github.io/match09/match09/samples012/ㄘ.png##https://anne201.github.io/match09/match09/samples012/ㄘ.mp3
+https://anne201.github.io/match09/match09/samples012/ㄙ.png##https://anne201.github.io/match09/match09/samples012/ㄙ.mp3
+-----*/}.toString().replace(/\r/g,"").slice("function(){/*--這一行請勿更改--".length+1,-9);
+
